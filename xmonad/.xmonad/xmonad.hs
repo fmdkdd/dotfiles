fd042b30a6f4d1128c7ab97932a2f37f4364f144
fmdkdd <fmdkdd@gmail.com>
2014-08-17 20:18:30 +0200

    XMonad: do not display border when full screen

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
 
 import qualified DBus as D
 import qualified DBus.Client as D
@@ -12,6 +13,20 @@ import qualified Codec.Binary.UTF8.String as UTF8
 myManageHook = composeAll
     [ className =? "Do"        --> doFloat
     ]
+
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
 
 main :: IO ()
 main = do
@@ -22,7 +37,8 @@ main = do
          modMask = mod4Mask,
          normalBorderColor  = "#666",
          focusedBorderColor = "#e3efc0",
-         manageHook         = myManageHook <+> manageDocks
+         manageHook         = myManageHook <+> manageDocks,
+         layoutHook         = avoidStruts $ smartBorders $ myLayout
        }
 
 prettyPrinter :: D.Client -> PP
