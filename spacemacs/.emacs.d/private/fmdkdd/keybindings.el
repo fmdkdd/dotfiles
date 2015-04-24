da7fc359379c75399fefd88e7b8c37a843aa1ed0
fmdkdd <fmdkdd@gmail.com>
2015-04-24 09:46:43 +0200

    Org mode keybindings from startup
    contrib/org layer only binds them after an org-mode buffer is opened, or
    after a call to helm M-x.

---
+++ spacemacs/.emacs.d/private/fmdkdd/keybindings.el
@@ -0,0 +1,12 @@
+;;; funcs.el --- fmdkdd Layer key bindings File
+;;
+;; Copyright (c) 2015 fmdkdd
+;;
+;; Author: fmdkdd
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
