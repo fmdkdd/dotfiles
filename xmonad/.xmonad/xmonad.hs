21f061a7036294c199efb77b172776b28708645d
fmdkdd <fmdkdd@gmail.com>
2015-05-11 13:17:02 +0200

    XMonad Colemak bindings, colors

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import XMonad
+import XMonad.Actions.Warp
 import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig (additionalKeys)
 
 import qualified DBus as D
 import qualified DBus.Client as D
 import qualified Codec.Binary.UTF8.String as UTF8
+import qualified XMonad.StackSet as W
 
 myManageHook = composeAll [
   className =? "Do"        --> doFloat
@@ -33,23 +36,39 @@ main :: IO ()
 main = do
     dbus <- D.connectSession
     getWellKnownName dbus
-    xmonad $ gnomeConfig
-       { logHook = dynamicLogWithPP (prettyPrinter dbus),
-         modMask = mod4Mask,
-         normalBorderColor  = "#666",
-         focusedBorderColor = "#e3efc0",
-         manageHook         = myManageHook <+> manageDocks,
-         layoutHook         = avoidStruts $ smartBorders $ myLayout
-       }
+    (xmonad $ gnomeConfig
+     { logHook = dynamicLogWithPP (prettyPrinter dbus),
+       modMask = mod4Mask,
+       normalBorderColor  = "#eee8d5",
+       focusedBorderColor = "#268bd2",
+       manageHook         = myManageHook <+> manageDocks,
+       layoutHook         = avoidStruts $ smartBorders $ myLayout
+     }
+     `additionalKeys`
+     ([
+         ((mod4Mask, xK_h), windows W.focusUp)
+      , ((mod4Mask, xK_k), windows W.focusDown)
+      , ((mod4Mask .|. shiftMask, xK_h), windows W.swapUp)
+      , ((mod4Mask .|. shiftMask, xK_k), windows W.swapDown)
+      , ((mod4Mask, xK_j), sendMessage Shrink)
+      , ((mod4Mask .|. shiftMask, xK_Delete), spawn "gnome-session-quit --power-off")
+      , ((mod4Mask .|. shiftMask, xK_q), spawn "gnome-session-quit")
+      , ((mod4Mask, xK_w), warpToWindow 0.5 0.5)
+      ]
+      ++
+      [((m .|. mod4Mask, key), do
+           screenWorkspace sc >>= flip whenJust (windows . f))
+      | (key, sc) <- zip [xK_n, xK_e, xK_i] [0..]
+      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]))
 
 prettyPrinter :: D.Client -> PP
 prettyPrinter dbus = defaultPP
     { ppOutput = dbusOutput dbus
     , ppTitle = pangoSanitize
     , ppCurrent = \x -> pangoSanitize (x ++ " ::")
-    , ppVisible = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
+    , ppVisible = pangoColor "#268bd2" . wrap "(" ")" . pangoSanitize
     , ppHidden = const ""
-    , ppUrgent = pangoColor "red"
+    , ppUrgent = pangoColor "#d33682"
     , ppLayout = const ""
     , ppSep = " "
     }
