cdcba46a467a70d928e3b4275938f349be665f14
fmdkdd <fmdkdd@gmail.com>
2015-08-18 11:24:36 +0200

    Spacing in xmonad.hs

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -24,17 +24,17 @@ myManageHook = composeAll [
 
 myLayout = tiled ||| Mirror tiled ||| Full
   where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
 
-     -- The default number of windows in the master pane
-     nmaster = 1
+    -- The default number of windows in the master pane
+    nmaster = 1
 
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
 
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
 
 data Theme = Light | Dark deriving (Show, Read)
 
@@ -50,78 +50,77 @@ loadTheme :: IO Theme
 loadTheme = do
   themeEnv <- lookupEnv "solarized"
   let themeName = toggleTheme $ maybe Light read themeEnv
-  putEnv $ "solarized=" ++ (show themeName)
-  spawn ("/usr/local/bin/gnome-terminal-solarized " ++ (map toLower (show themeName)))
+  putEnv $ "solarized=" ++ show themeName
+  spawn ("/usr/local/bin/gnome-terminal-solarized " ++ map toLower (show themeName))
   return themeName
 
 
 main :: IO ()
 main = do
-    dbus <- D.connectSession
-    getWellKnownName dbus
-    theme <- loadTheme
-    (xmonad $ gnomeConfig
-     { logHook = dynamicLogWithPP (prettyPrinter dbus),
-       modMask = mod4Mask,
-       normalBorderColor  = borderColor theme,
-       focusedBorderColor = "#268bd2",
-       manageHook         = myManageHook <+> manageDocks,
-       layoutHook         = avoidStruts $ smartBorders $ myLayout,
-       handleEventHook = fullscreenEventHook
+  dbus <- D.connectSession
+  getWellKnownName dbus
+  theme <- loadTheme
+  xmonad $ gnomeConfig
+    { logHook            = dynamicLogWithPP (prettyPrinter dbus),
+      modMask            = mod4Mask,
+      normalBorderColor  = borderColor theme,
+      focusedBorderColor = "#268bd2",
+      manageHook         = myManageHook <+> manageDocks,
+      layoutHook         = avoidStruts $ smartBorders myLayout,
+      handleEventHook    = fullscreenEventHook
      }
-     `additionalKeys`
-     ([
-         ((mod4Mask, xK_h), windows W.focusUp)
-      , ((mod4Mask, xK_k), windows W.focusDown)
-      , ((mod4Mask .|. shiftMask, xK_h), windows W.swapUp)
-      , ((mod4Mask .|. shiftMask, xK_k), windows W.swapDown)
-      , ((mod4Mask, xK_j), sendMessage Shrink)
-      , ((mod4Mask .|. shiftMask, xK_Delete), spawn "gnome-session-quit --power-off")
-      , ((mod4Mask .|. shiftMask, xK_q), spawn "gnome-session-quit")
-      , ((mod4Mask, xK_w), warpToWindow 0.5 0.5)
-      ]
-      ++
-      [((m .|. mod4Mask, key), do
-           screenWorkspace sc >>= flip whenJust (windows . f))
-      | (key, sc) <- zip [xK_n, xK_e, xK_i] [0..]
-      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]))
+    `additionalKeys`
+    ([
+         ((mod4Mask, xK_h), windows W.focusUp),
+         ((mod4Mask, xK_k), windows W.focusDown),
+         ((mod4Mask .|. shiftMask, xK_h), windows W.swapUp),
+         ((mod4Mask .|. shiftMask, xK_k), windows W.swapDown),
+         ((mod4Mask, xK_j), sendMessage Shrink),
+         ((mod4Mask .|. shiftMask, xK_Delete), spawn "gnome-session-quit --power-off"),
+         ((mod4Mask .|. shiftMask, xK_q), spawn "gnome-session-quit"),
+         ((mod4Mask, xK_w), warpToWindow 0.5 0.5)
+     ]
+     ++
+     [((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+     | (key, sc) <- zip [xK_n, xK_e, xK_i] [0..],
+       (f, m) <- [(W.view, 0), (W.shift, shiftMask)]])
 
 prettyPrinter :: D.Client -> PP
-prettyPrinter dbus = defaultPP
-    { ppOutput = dbusOutput dbus
-    , ppTitle = pangoSanitize
-    , ppCurrent = \x -> pangoSanitize (x ++ " ::")
-    , ppVisible = pangoColor "#268bd2" . wrap "(" ")" . pangoSanitize
-    , ppHidden = const ""
-    , ppUrgent = pangoColor "#d33682"
-    , ppLayout = const ""
-    , ppSep = " "
-    }
+prettyPrinter dbus =
+  defaultPP
+  { ppOutput = dbusOutput dbus
+  , ppTitle = pangoSanitize
+  , ppCurrent = \x -> pangoSanitize (x ++ " ::")
+  , ppVisible = pangoColor "#268bd2" . wrap "(" ")" . pangoSanitize
+  , ppHidden = const ""
+  , ppUrgent = pangoColor "#d33682"
+  , ppLayout = const ""
+  , ppSep = " "
+  }
 
 getWellKnownName :: D.Client -> IO ()
 getWellKnownName dbus = do
   D.requestName dbus (D.busName_ "org.xmonad.Log")
-                [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+    [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
   return ()
 
 dbusOutput :: D.Client -> String -> IO ()
 dbusOutput dbus str = do
-    let signal = (D.signal "/org/xmonad/Log" "org.xmonad.Log" "Update") {
-            D.signalBody = [D.toVariant ("<b>" ++ (UTF8.decodeString str) ++ "</b>")]
-        }
-    D.emit dbus signal
+  let signal = (D.signal "/org/xmonad/Log" "org.xmonad.Log" "Update") {
+        D.signalBody = [D.toVariant ("<b>" ++ UTF8.decodeString str ++ "</b>")]}
+  D.emit dbus signal
 
 pangoColor :: String -> String -> String
 pangoColor fg = wrap left right
   where
-    left = "<span foreground=\"" ++ fg ++ "\">"
+    left  = "<span foreground =\"" ++ fg ++ "\">"
     right = "</span>"
 
 pangoSanitize :: String -> String
 pangoSanitize = foldr sanitize ""
   where
-    sanitize '>' xs = "&gt;" ++ xs
-    sanitize '<' xs = "&lt;" ++ xs
+    sanitize '>'  xs = "&gt;" ++ xs
+    sanitize '<'  xs = "&lt;" ++ xs
     sanitize '\"' xs = "&quot;" ++ xs
-    sanitize '&' xs = "&amp;" ++ xs
-    sanitize x xs = x:xs
+    sanitize '&'  xs = "&amp;" ++ xs
+    sanitize x    xs = x:xs
