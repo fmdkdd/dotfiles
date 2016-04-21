72051ecac2ca63859fb311742deffab7a9932b11
fmdkdd <fmdkdd@gmail.com>
2016-04-21 17:12:36 +0200

    Enter the fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,6 @@
-set PATH ~/.cabal/bin $PATH
-set NO_AT_BRIDGE 1 # Fix for accessibility bus warning when launching evince, sunvox ...
+# Source /etc/profile and /etc/profile.d files
+env -i HOME=$HOME dash -l -c printenv | sed -e '/PATH/s/:/ /g;s/=/ /;s/^/set -x /' | source
+
+
+#set PATH ~/.cabal/bin $PATH
+#set NO_AT_BRIDGE 1 # Fix for accessibility bus warning when launching evince, sunvox ...
