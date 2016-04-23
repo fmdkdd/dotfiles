02b43aa4ea6b2996e7a9c1c7b161978a184aa736
fmdkdd <fmdkdd@gmail.com>
2016-04-23 16:50:32 +0200

    Add node modules to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 # Source /etc/profile and /etc/profile.d files
 env -i HOME=$HOME dash -l -c printenv | sed -e '/PATH/s/:/ /g;s/=/ /;s/^/set -x /' | source
 
+set PATH $PATH ~/.node_modules/bin 
 
 #set PATH ~/.cabal/bin $PATH
 #set NO_AT_BRIDGE 1 # Fix for accessibility bus warning when launching evince, sunvox ...
