d5fce48ed102b538356a885a7e8c90d573b4e58c
fmdkdd <fmdkdd@gmail.com>
2016-04-25 15:52:28 +0200

    Remove fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,10 @@
 # Source /etc/profile and /etc/profile.d files
 env -i HOME=$HOME dash -l -c printenv | sed -e '/PATH/s/:/ /g;s/=/ /;s/^/set -x /' | source
 
-set PATH $PATH ~/.node_modules/bin 
-
+set PATH $PATH ~/.node_modules/bin
 #set PATH ~/.cabal/bin $PATH
+
+# Remove greeting
+set fish_greeting
+
 #set NO_AT_BRIDGE 1 # Fix for accessibility bus warning when launching evince, sunvox ...
