e24d7b4d7e2b68216090f5ca6b82f668c1e0b75a
fmdkdd <fmdkdd@gmail.com>
2016-04-28 17:47:09 +0200

    Fix files with spaces in names

--- rofi/.config/rofi/papers.sh
+++ rofi/.config/rofi/papers.sh
@@ -6,7 +6,9 @@ if [ -z $@ ]; then
 else
     PAPER=$@
 
-    if [ -f ${PAPER} ]; then
+    echo $PAPER >> /tmp/ttt.log
+
+    if [ -f "${PAPER}" ]; then
         exec zathura "${PAPER}" 1>&- # Close stdout otherwise rofi hangs around
         # and steals input
     fi
