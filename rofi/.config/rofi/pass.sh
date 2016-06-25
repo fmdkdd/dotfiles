ccaeccd5f259e0c4fd31caa40cc11fab02e47cce
fmdkdd <fmdkdd@gmail.com>
2016-06-25 16:10:10 +0200

    [rofi] Handle multi-line pass files

--- rofi/.config/rofi/pass.sh
+++ rofi/.config/rofi/pass.sh
@@ -9,6 +9,10 @@ if [ -z $@ ]; then
 else
     ACCOUNT=$@
 
+    # Show extended info (but *not* the password which is on first line)
+    pass show "$ACCOUNT" | sed -n '1!p'
+
     # Copy password to primary X selection (middle-mouse paste)
-    exec env PASSWORD_STORE_X_SELECTION=primary pass show --clip "$ACCOUNT" 1>&-
+    exec env PASSWORD_STORE_X_SELECTION=primary PASSWORD_STORE_CLIP_TIME=10 \
+         pass show --clip "$ACCOUNT" 1>&-
 fi
