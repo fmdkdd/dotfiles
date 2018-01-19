9fe960ae7aef2b9be33c26b68d00d33539a7a15e
fmdkdd <fmdkdd@0xc0de.fr>
2018-01-19 12:27:43 +0100

    Start ssh-agent with startx

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,6 +15,6 @@ abbr -a r ranger
 abbr -a pacup sudo pacman -Syu
 abbr -a pacs sudo pacman -S
 abbr -a pacss sudo pacman -Ss
-abbr -a startx startx -- -ardelay 200 -arinterval 30
+abbr -a startx ssh-agent startx -- -ardelay 200 -arinterval 30
 
 #set NO_AT_BRIDGE 1 # Fix for accessibility bus warning when launching evince, sunvox ...
